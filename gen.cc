6164bcc60b1b67f926b0d95365bb6bbb24b822f5
etyguo <etyguo@gmail.com>
2015-09-30 23:35:21 -0400

    Update gen.cc

--- gen.cc
+++ gen.cc
@@ -90,7 +90,8 @@ gen::gen(const edm::ParameterSet& iConfig)
 
 {
    //now do what ever initialization is needed
- histWpt=fs->make<TH1F>("Wpt_gen","W+ pt",10,0,10);
+   Service<TFileService> fs;
+   histWpt=fs->make<TH1F>("Wpt_gen","W+ pt",10,0,10);
 }
 
 
@@ -115,7 +116,255 @@ gen::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    using namespace reco;
    Handle<GenParticleCollection> genParticles;
    iEvent.getByLabel("genParticles", genParticles);
-   
+   for(size_t i = 0; i < genParticles->size(); ++ i) //starting from tau analysis_gen 
+	{
+		  int final=1;
+			int count_muon=0;
+			const GenParticle & p = (*genParticles)[i];  //geting the partilce as p
+	   			if(p.pdgId()==15 || p.pdgId()== -15 ) //tau is 15
+					{
+							int k = p.numberOfDaughters();
+	   					int   n=p.numberOfMothers();
+							for (int j=0; j<n; ++j)
+							{
+								const Candidate * mon=p.mother(j);
+								int monId=mon->pdgId();
+								if (monId != 23 && monId!=p.pdgId())
+								{
+									final=0;
+									break;
+								}
+							}
+							for (int j=0;j<k;++ j)
+							{
+								const Candidate * d = p.daughter(j);
+       					int dauId = d->pdgId();
+								if (dauId==p.pdgId())
+								{
+									final=0;
+								}
+							}
+							if (final == 1)
+							{
+								histZchild->Fill(p.pdgId());
+							}
+							for(int j = 0; j < k; ++ j) 
+								{
+       					const Candidate * d = p.daughter(j);
+       					int dauId = d->pdgId();
+								if (dauId != 13 && dauId != -13)
+								{
+									final=0;
+									break;
+								}
+								if (dauId == 13 || dauId == -13)
+								{
+									count_muon++;
+								}
+								}
+							
+							if (p.pdgId()==-15 && count_muon==3)
+							{
+								positive=0;
+							}	
+						histWpt->Fill(k);	
+            if (final==1 && count_muon==3)
+							{
+						//		int k = p.numberOfDaughters();
+								histtaupt->Fill(p.pt());
+								histtaueta->Fill(p.eta());			
+								histtaumass->Fill(p.mass());
+								} //end of if (final==1 && count_muon==3)
+				}	//end of if if pdg.ID
+	}//end of Tau Analysis.
+	if (positive == 1) 
+{
+NoGenMuon=0;
+for(size_t i = 0; i < genParticles->size(); ++ i) //starting from muon analysis_gen for muon
+	{
+		  int final=1;
+			const GenParticle & p = (*genParticles)[i];  //geting the partilce as p
+	//   	const Candidate * mon=p.mother();
+	   	int   n=p.numberOfMothers();
+			int   k=p.numberOfDaughters();
+	
+				if(p.pdgId()==13 || p.pdgId()==-13 ) //muon is 13
+					{
+				
+						histmuongenallpt->Fill(p.pt());
+						histmuongenalleta->Fill(p.eta());
+						for (int j=0; j<n; ++j)
+							{
+								const Candidate * mon=p.mother(j);
+								int monId=mon->pdgId();
+                const Candidate * grandmon=mon->mother(0);
+								int grandmonId=grandmon->pdgId();
+								if (monId != 15 || k>0)
+								{
+									final=0;
+								}
+								if (grandmonId!=23 && grandmonId!=15)
+								{
+									final=0;
+								}
+							}
+						int child=1;
+		
+						for (int j=0;j<k;++j)
+						{
+							const Candidate * child=p.daughter(j);
+						  if (child->pdgId() == p.pdgId())	
+							{
+								child=0;
+								break;
+							}
+						}
+				
+						if (final ==0 && child ==1)
+						{
+						 	gen_other_pt[count_other_muon]=p.pt();
+						 	gen_other_eta[count_other_muon]=p.eta();
+						 	gen_other_phi[count_other_muon]=p.phi();
+						 	gen_other_px[count_other_muon]=p.px();
+						 	gen_other_py[count_other_muon]=p.py();
+						 	gen_other_pz[count_other_muon]=p.pz();
+						 	gen_other_energy[count_other_muon]=p.energy();
+							gen_other_mother[count_other_muon]=p.mother(0)->pdgId();
+							count_other_muon++;		
+							histgenothermumother->Fill(p.mother(0)->pdgId());
+							if (p.mother(0)->pdgId()==-15)
+							{
+								tau_px=p.mother(0)->px();		
+								tau_py=p.mother(0)->py();		
+								tau_pz=p.mother(0)->pz();		
+								tau_energy=p.mother(0)->energy();		
+								tau_pt=p.mother(0)->pt();
+								tau_eta=p.mother(0)->eta();
+								tau_phi=p.mother(0)->phi();
+							}
+						}
+						if (final ==1 && n==1)
+						{
+								histmugenpt->Fill(p.pt());
+								histmugeneta->Fill(p.eta());
+								histmugenphi->Fill(p.phi());
+								pt_gen[count]=p.pt();
+								eta_gen[count]=p.eta();
+								phi_gen[count]=p.phi();
+								charge_gen[count]=p.pdgId();
+								count++;
+								if (p.pt()>3.0 && p.eta()>-2.4 && p.eta()<2.4)
+								{	
+								histmuongencutpt->Fill(p.pt());
+								histmuongencuteta->Fill(p.eta());
+								gen_muon_eta[NoGenMuon]=p.eta();
+                gen_muon_pt[NoGenMuon]=p.pt();
+								gen_muon_phi[NoGenMuon]=p.phi();
+		//						gen_muon_charge[NoGenMuon]=p.pdgId();
+								px_g=px_g+p.px();
+								py_g=py_g+p.py();
+								pz_g=pz_g+p.pz();
+								energy_g=energy_g+p.energy();
+								NoGenMuon++;
+					  		}
+						}
+					}
+    
+	} //end of gen_loop_3
+}
+//cout<<"fuck 8"<<endl;
+if (positive == 0)  // analysis for the tau+ -15
+{
+		NoGenMuon=0;
+    for(size_t i = 0; i < genParticles->size(); ++ i) //starting from muon analysis_gen for tau+
+    {
+        int final=1;
+        const GenParticle & p = (*genParticles)[i];  //geting the partilce as p
+        //   	const Candidate * mon=p.mother();
+        int   n=p.numberOfMothers();
+				int   k=p.numberOfDaughters();
+        if(p.pdgId()==13 || p.pdgId()==-13 ) //muon is 13
+        {
+            histmuongenallpt->Fill(p.pt());
+            histmuongenalleta->Fill(p.eta());
+            for (int j=0; j<n; ++j)
+            {
+                const Candidate * mon=p.mother(j);
+                int monId=mon->pdgId();
+                const Candidate * grandmon=mon->mother(0);
+                if (grandmon->pdgId() != 23 && grandmon->pdgId() != -15)
+								{
+								 final=0;
+								}
+								if (monId != -15 || k>0)
+                {
+                    final=0;
+                }
+            
+						}
+						int child=1;
+						for (int j=0;j<k;++j)
+						{
+							const Candidate * child=p.daughter(j);
+						  if (child->pdgId() == p.pdgId())	
+							{
+								child=0;
+								break;
+							}
+						}
+						if (final == 0 && child ==1)
+						{
+						 	gen_other_pt[count_other_muon]=p.pt();
+						 	gen_other_eta[count_other_muon]=p.eta();
+						 	gen_other_phi[count_other_muon]=p.phi();
+							gen_other_mother[count_other_muon]=p.mother(0)->pdgId();
+							gen_other_px[count_other_muon]=p.px();
+						 	gen_other_py[count_other_muon]=p.py();
+						 	gen_other_pz[count_other_muon]=p.pz();
+						 	gen_other_energy[count_other_muon]=p.energy();
+							count_other_muon++;		
+							histgenothermumother->Fill(p.mother(0)->pdgId());
+							if (p.mother(0)->pdgId()==15)
+							{
+								tau_px=p.mother(0)->px();		
+								tau_py=p.mother(0)->py();		
+								tau_pz=p.mother(0)->pz();		
+								tau_energy=p.mother(0)->energy();		
+								tau_pt=p.mother(0)->pt();
+								tau_eta=p.mother(0)->eta();
+								tau_phi=p.mother(0)->phi();
+
+							}
+						}
+            if (final ==1 && n==1)
+            {
+                pt_gen[count]=p.pt();
+                eta_gen[count]=p.eta();
+                phi_gen[count]=p.phi();
+                charge_gen[count]=p.pdgId();
+                count++;
+                histmugenpt->Fill(p.pt());
+                histmugeneta->Fill(p.eta());
+                histmugenphi->Fill(p.phi());
+								
+                if (p.pt()>3.0 && p.eta()>-2.4 && p.eta()<2.4)
+                {
+                    histmuongencutpt->Fill(p.pt());
+                    histmuongencuteta->Fill(p.eta());
+                    gen_muon_pt[NoGenMuon]=p.pt();
+                    gen_muon_eta[NoGenMuon]=p.eta();
+                    gen_muon_phi[NoGenMuon]=p.phi();
+      //              gen_muon_charge[NoGenMuon]=p.pdgId();
+                    px_g=px_g+p.px();
+                    py_g=py_g+p.py();
+                    pz_g=pz_g+p.pz();
+                    energy_g=energy_g+p.energy();
+                    NoGenMuon++;
+                }
+            }
+        }
+    } //end of gen_loop_4
+} //end if positive 
 #ifdef THIS_IS_AN_EVENT_EXAMPLE
    Handle<ExampleData> pIn;
    iEvent.getByLabel("example",pIn);
